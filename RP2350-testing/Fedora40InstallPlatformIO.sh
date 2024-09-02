b680d437fa8b5ddc9bbd1b1eb0f6383313abf6fc
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-09-02 14:41:39 -0700

    working in a Fedora40 container

---
+++ RP2350-testing/Fedora40InstallPlatformIO.sh
@@ -0,0 +1,25 @@
+#! /usr/bin/env bash
+
+set -e
+
+source ./set_envars
+
+echo "Installing Linux dependencies"
+sudo dnf install -y \
+  gcc \
+  glibc-devel.i686 \
+  screen \
+  usbutils \
+  > $LOGFILES/dnf.log 2>&1
+
+echo "Creating fresh $PIO_VENV"
+rm -fr $PIO_VENV
+python3 -m venv --upgrade-deps $PIO_VENV
+
+echo "Installing PlatformIO in $PIO_VENV"
+source $PIO_VENV/bin/activate
+pip3 install --upgrade platformio \
+  > $LOGFILES/platformio.log 2>&1
+deactivate
+
+echo "Finished"
