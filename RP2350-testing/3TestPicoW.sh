1d336321beab9fd9be0fb8969a20f821afbdfe1c
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-09-08 14:09:57 -0700

    enhanced logging

--- RP2350-testing/3TestPicoW.sh
+++ RP2350-testing/3TestPicoW.sh
@@ -2,27 +2,33 @@
 
 set -e
 
-echo "If the upload fails, you will have to put the device"
-echo "in BOOTSEL mode and re-run."
-echo ""
-echo "    picotool info -a -F"
-echo ""
-echo "should do it once you've done a successful upload."
-echo "If you get no response that way, you'll need to do"
-echo "it manually."
-echo ""
-echo "Sleeping 20 seconds in case you need to CTL-C and restart."
-sleep 20
-
 source ./set_envars
 source $PIO_VENV/bin/activate
+export LOGFILE=$LOGFILES/test-pico-w-cforth.log
+rm $LOGFILE
 
 pushd ..
-  echo "Building and uploading Pico W cforth"
-  pio run --verbose \
+  echo "Listing USB devices"
+  lsusb \
+    >> $LOGFILE 2>&1
+  echo "Listing relevant TTYs"
+  ls -l /dev/ttyACM* /dev/ttyUSB* || true \
+    >> $LOGFILE 2>&1
+  date \
+    >> $LOGFILE 2>&1
+  echo "Building and uploading"
+  /usr/bin/time pio run --verbose \
     --environment test_pico_w \
     --target upload \
-    2>&1 | tee $LOGFILES/test-pico-w-cforth.log 2>&1
+    >> $LOGFILE 2>&1
+  date \
+    >> $LOGFILE 2>&1
+  echo "Listing USB devices"
+  lsusb \
+    >> $LOGFILE 2>&1
+  echo "Listing relevant TTYs"
+  ls -l /dev/ttyACM* /dev/ttyUSB* || true \
+    >> $LOGFILE 2>&1
 popd
 
 deactivate
