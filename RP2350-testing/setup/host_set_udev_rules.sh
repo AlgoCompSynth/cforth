810e879235ed720eb7540b263681a3ae47216fd3
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-09-12 12:14:08 -0700

    Consolidate Arduino and PlatformIO udev rules

--- RP2350-testing/setup/host_set_udev_rules.sh
+++ RP2350-testing/setup/host_set_udev_rules.sh
@@ -4,7 +4,10 @@ set -e
 
 echo ""
 echo "Setting environment variables"
-source ../set_pico_envars
+source ../set_envars
+
+echo "Installing PlatformIO rules as 'root'!!"
+curl -fsSL $PLATFORMIO_UDEV_RULES_URL | sudo tee $PLATFORMIO_UDEV_RULES_PATH
 
 echo "Installing Arduino MBED RP2040 'udev' rules as 'root'!!"
 sudo cp 60-arduino-mbed.rules $SYSTEM_UDEV_PATH/
